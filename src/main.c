2d9c4999cf24bf4ff9578b0009680624c6fdf5bd
LinkWu <wjianl@qq.com>
2026-03-02 20:01:24 +0800

    chore: 编辑模板代码

--- src/main.c
+++ src/main.c
@@ -2,30 +2,10 @@
 #include <Microbit_Matrix.h>
 #include <Microbit_Sensors.h>
 
-void joy_stick() {
-
-}
-
-void onRadioReceive(String radio_message) {
-}
-
+// 主程序开始
 void setup() {
-    uint8_t a = 0;
-    dfrobotRandomSeed();
-    Radio.setCallback(onRadioReceive);
-	Radio.turnOn();
-	Radio.setGroup(7);
-	Serial.begin(9600);
-	MMatrix.drawPixel(random(0l), 0, LED_ON);
-	MMatrix.show(MMatrix.EMPTYHEART);
-	random(0l);
-	String("0").toInt();
-	analogRead(1);
-	Button_A.isPressed();
-}
 
+}
 void loop() {
-    if (Button_A.isPressed()) {
-        Radio.send("A");
-    }
+
 }
